b8b411cdbc66d4e0cfeaeab0e58cfd65a44d0212
Chatchapong Thuaypha <chatchapong1@windowslive.com>
2024-10-23 19:07:28 -0700

    Completed Program

---
+++ Activity23/function.cpp
@@ -0,0 +1,8 @@
+//
+//  function.cpp
+//  Activity23
+//
+//  Created by Chatchapong Thuaypha on 10/23/24.
+//
+
+#include <stdio.h>
---
+++ Activity23/header.h
@@ -0,0 +1,12 @@
+//
+//  header.h
+//  Activity23
+//
+//  Created by Chatchapong Thuaypha on 10/23/24.
+//
+
+#ifndef header_h
+#define header_h
+
+
+#endif /* header_h */
